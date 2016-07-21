449284ea71d9c175087f24cdc0797a70e5c38ca0
Amin Oruji <amin@oruji.org>
2016-07-22 02:48:46 +0430

    add decoder and encoder

--- static/js/editor.js
+++ static/js/editor.js
@@ -2,21 +2,31 @@ $(document).ready(function() {
 	var myText = $("#myTextArea");
 	var myP = $("#myP");
 	var myB = $("#myB");
-	
+
 	myText.hide();
 	myB.hide();
-	
+
 	// click on p
 	myP.click(function() {
 		myText.show();
 		myB.show();
-		myText.val(myP.html());
+		myText.val(myEncode(myP.html()));
 	});
-	
+
 	// save changes
 	myB.click(function() {
 		myText.hide();
 		myB.hide();
-		myP.html(myText.val());
+		myP.html(myDecode(myText.val()));
 	});
 });
+
+function myDecode(myStr) {
+	return myStr.replace("*کج*", "<bdo class=\"leftInline\">").replace(
+			"*/کج*", "</bdo>")
+}
+
+function myEncode(myStr) {
+	return myStr.replace("<bdo class=\"leftInline\">", "*کج*").replace(
+			"</bdo>", "*/کج*");
+}
