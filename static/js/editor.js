5c004d574b3e85e2794154b00cad8109327699c5
Amin Oruji <amin@oruji.org>
2016-07-22 02:26:31 +0430

    add change event

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
 	var myText = $("#myTextArea");
 	var myP = $("#myP");
+	var myB = $("#myB");
+	
 	myText.hide();
+	myB.hide();
+	
+	// click on p
 	myP.click(function() {
 		myText.show();
+		myB.show();
 		myText.val(myP.html());
+	});
+	
+	// save changes
+	myB.click(function() {
+		myText.hide();
+		myB.hide();
+		myP.html(myText.val());
 	});
 });
